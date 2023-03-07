82ca7a4a849a34e4506010bb0710c6e2d1ce9dc0
dev <dev@gmail.com>
2023-03-07 21:37:14 +0530

    Practice new

---
+++ src/Denny.java
@@ -0,0 +1,13 @@
+public class Denny {
+    public int add(int a,int b)
+    {
+     int c=a-b;
+     return c;
+    }
+    public static void main(String [] args)
+    {
+        Denny nik=new Denny();
+        int y=nik.add(22,77);
+        System.out.println(y);
+    }
+}
