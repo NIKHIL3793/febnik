fa0a5474bba6a7636bcea08593575f0022d14a38
dev <dev@gmail.com>
2023-03-16 13:13:34 +0530

    Practice new

---
+++ src/Arrays/array1.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class array1 {
+    public static void main(String[] args) {
+        int c[]=new int[4];
+         c[0]=1;
+         c[1]=2;
+         c[2]=3;
+         c[3]=4;
+         for (int i=0;i<=c.length-1;i++){
+             System.out.println(c[0]);
+         }
+
+    }
+}
