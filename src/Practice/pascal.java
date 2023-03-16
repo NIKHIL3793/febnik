3ed1aca2c4df77a7b88e289603eacf955faca1fb
dev <dev@gmail.com>
2023-03-16 13:12:02 +0530

    Practice new

---
+++ src/Practice/pascal.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class pascal
+{
+    public static void main(String[] args) {
+        for (int i=1;i<=6;i++){
+            for (int j=1;j<=(6-1);j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
